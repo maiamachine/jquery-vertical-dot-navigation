45c362a2296679563b8d440ace59e5c19a975a92
maiachristina <maiachristinalee@gmail.com>
2015-11-25 22:57:53 -0500

    added default options and customizations

--- js/jq-vertical-dot-nav.js
+++ js/jq-vertical-dot-nav.js
@@ -1,11 +1,24 @@
 (function ( $ ) {
- 
-    $.fn.verticalDotNav = function() {
 
-    	var sections_arr = [],
+	var default_options = {
+		align : "right", 
+		scroll_speed : 1000,
+		dot_size: 10,
+		dot_style: "circle",
+		dot_color: "#fff",
+		nav_color: "#666"
+	}
+ 
+    $.fn.verticalDotNav = function(options) {
+
+    	$.extend(default_options, options);
+
+    	var nav_height,
+    		nav_pos_top, 
+    		nav_styles = {},
     		nav = "<ul class='vertical-dot-nav'>",
-    		nav_height = 0,
-    		nav_pos_top = 0,
+    		dot_styles = {},
+    		sections_arr = [],
     		window_height = $(window).height();
 
  
@@ -25,19 +38,50 @@
         
         $("body").append(nav);
 
+        dot_styles["width"] = default_options.dot_size + "px";
+       	dot_styles["height"] = default_options.dot_size + "px";
+       	dot_styles["border-color"] = default_options.dot_color;
+
+        if(default_options.dot_style === "circle") {
+        	dot_styles["border-radius"] = "50%";
+        }
+
+        $(".vertical-dot-nav .dot").css(dot_styles);
+       
         nav_height = $(".vertical-dot-nav").height();
         nav_pos_top = (window_height/2) - (nav_height/2);
+        nav_styles["top"] = nav_pos_top + "px";
+        nav_styles["background-color"] = default_options.nav_color;
+ 
+        if(default_options.align === "left") {
+        	nav_styles["left"] = 0;
+        	nav_styles["border-radius"] = "0 10px 10px 0";
+        } else {
+        	nav_styles.right = 0;
+        	nav_styles["border-radius"] = "10px 0 0 10px";
+        }
 
-        $(".vertical-dot-nav").css("top", nav_pos_top + "px");
+    	$(".vertical-dot-nav").css(nav_styles);
 
         $(".vertical-dot-nav .dot").each(function(index){
+
+        	$(this).on("mouseover", function(){
+        		$(this).css("background-color", default_options.dot_color);
+        	}).on("mouseout", function(){
+        		$(this).css("background-color", "transparent");
+        	})
         	 
         	 $(this).on("click", function(){
+
         	 	var target_section = sections_arr[index].offset;
-        	 	 
+
+        		$(".vertical-dot-nav .dot").removeClass("active");
+        		$(this).addClass("active");
+        		$(".vertical-dot-nav .active").css("background-color", default_options.dot_color);
+ 	 
         	 	$('html,body').animate({
 			        scrollTop: target_section
-			    }, 1000); 	
+			    }, default_options.scroll_speed); 	
         	 })
         	 
         })
